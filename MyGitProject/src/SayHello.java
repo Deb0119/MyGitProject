c546a2e9e46caba6a71a105c5f24e890e0da7be0
Deb0119 <debalinadc87@gmail.com>
2017-03-20 17:08:20 -0400

    Update SayHello.java

--- MyGitProject/src/SayHello.java
+++ MyGitProject/src/SayHello.java
@@ -5,8 +5,8 @@ public class SayHello {
 		 System.out.println("This is my 1st message");
 		 System.out.println("This is my 2nd message");
 		 System.out.println("This is my 3rd message");
-		 
-		 System.out.println("Hello Message");
+		 for(int i=0;i<5;i++)
+		  System.out.println("Hello Message");
 
 	}
 
