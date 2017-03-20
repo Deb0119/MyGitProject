cdcd02f7c17f89e4658d006344d39430ea9d8338
Deb0119 <debalinadc87@gmail.com>
2017-03-20 16:50:43 -0400

    commit 3rd message

--- MyGitProject/src/SayHello.java
+++ MyGitProject/src/SayHello.java
@@ -4,6 +4,7 @@ public class SayHello {
 	public static void main(String[] args) {
 		 System.out.println("This is my 1st message");
 		 System.out.println("This is my 2nd message");
+		 System.out.println("This is my 3rd message");
 
 	}
 
