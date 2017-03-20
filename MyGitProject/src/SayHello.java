7520408c1cbed9e7247449cf0b786a9e335125bb
Deb0119 <debalinadc87@gmail.com>
2017-03-20 16:30:19 -0400

    Commit 2nd message

--- MyGitProject/src/SayHello.java
+++ MyGitProject/src/SayHello.java
@@ -2,7 +2,8 @@
 public class SayHello {
 
 	public static void main(String[] args) {
-		 System.out.println("This ismy 1st message");
+		 System.out.println("This is my 1st message");
+		 System.out.println("This is my 2nd message");
 
 	}
 
