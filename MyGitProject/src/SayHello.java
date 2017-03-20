a26cae28df4e18eb63068a6e6dbd5fa876a17cf3
Deb0119 <debalinadc87@gmail.com>
2017-03-20 18:08:50 -0400

    commit

--- MyGitProject/src/SayHello.java
+++ MyGitProject/src/SayHello.java
@@ -6,7 +6,7 @@ public class SayHello {
 		 System.out.println("This is my 2nd message");
 		 System.out.println("This is my 3rd message");
 		 System.out.println("Hi Message");
-		 for(int i=0;i<4;i++)
+		 for(int i=1;i<4;i++)
 		 {
 		  System.out.println("Hello Message");
 		  System.out.println("Hi Message");
